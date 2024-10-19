7b2c17c63f05903389aaa49090074214bdf1da96
Josh Pavlovich <joshpavlovich@gmail.com>
2024-10-19 13:52:36 -0500

    Add Constants and TestOpClass from Emerald's project to bootstrap Onyx's drivetrain

---
+++ IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public final class Constants {
+    public final static class MotorConstants {
+        public static final double driveSpeed = 0.3;
+    }
+}
