56cbb6c5c1df65ee884b4fbd20c1e6ba9cb2d1a5
Karl Kay <kkay99@gmail.com>
2024-11-02 16:48:57 -0500

    Onyx Wrist/Intake working

--- IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -3,5 +3,11 @@ package org.firstinspires.ftc.teamcode;
 public final class Constants {
     public final static class MotorConstants {
         public static final double driveSpeed = 0.3;
+        public static final double viperMoveSpeed=0.8;
+        public static final double armMoveUpSpeed=-0.4;
+        public static final double armMoveDownSpeed=0.2;
+        public static final double intakeMoveSpeed=0.5;
+        public static final double wristMoveUpSpeed=0.5;
+        public static final double wristMoveDownSpeed=-0.3;
     }
 }
