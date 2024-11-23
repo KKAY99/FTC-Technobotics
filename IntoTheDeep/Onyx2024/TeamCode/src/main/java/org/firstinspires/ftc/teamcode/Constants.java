ea2c74e80071c65549b482dae37aad9334dddc25
Karl Kay <kkay99@gmail.com>
2024-11-23 12:47:51 -0600

    Updates for Arm

--- IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -9,6 +9,10 @@ public final class Constants {
         public static final double intakeMoveSpeed=0.5;
         public static final double wristMoveUpSpeed=0.5;
         public static final double wristMoveDownSpeed=-0.3;
-        public static final double armPositionUp=0;
+        public static final int armPositionUp=-500;
+        public static final int armPositionDown=500;
+        public static final double armAutoSpeed=0.6;
+        public static final int armMovementSpeed=20;
+        public static final int armMovementThreshold=50;
     }
 }
