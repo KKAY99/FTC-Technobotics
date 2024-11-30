dd7dcdb1cff5b0c4a9a5e1d146ada5ad6e772dc5
Alpha Sigma <tigerpotery@gmail.com>
2024-11-30 11:47:04 -0600

    hello

--- IntoTheDeep/Emerald2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Atonmous.java
+++ IntoTheDeep/Emerald2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Atonmous.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous
 public class Atonmous extends LinearOpMode {
+    @Override
     public void runOpMode() throws InterruptedException {
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
@@ -16,11 +19,46 @@ public class Atonmous extends LinearOpMode {
         DcMotor armMotor = hardwareMap.dcMotor.get("armMotor");
         DcMotor udarmMotor = hardwareMap.dcMotor.get("udarmMotor");
 
-
         Servo clawServo = hardwareMap.servo.get("clawServo");
-        Servo wristServo = hardwareMap.servo.get("wristServo");
+        CRServo wristServo = hardwareMap.crservo.get("wristServo");
 
+        waitForStart();
+        ElapsedTime timer=new ElapsedTime();
+        timer.reset();
 
+        double y=1;
+        double x=0;
+        double rx=0;
+        double maxPower = Constants.MotorConstants.driveSpeed;
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            y = y * maxPower;
+            x = x * maxPower;
+            rx = rx * maxPower;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(0 - frontLeftPower);
+            backLeftMotor.setPower(0 - backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+            if(timer.seconds() > 5){
+                frontLeftMotor.setPower(4);
+                frontRightMotor.setPower(4);
+                backLeftMotor.setPower(4);
+                backRightMotor.setPower(4);
+                stop();
+
+            }
+        }
 
     }
 }
