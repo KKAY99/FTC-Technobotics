570ca8931803814453c2ce0b8ca324f9618b62be
Karl Kay <kkay99@gmail.com>
2025-01-13 17:43:01 -0600

    updated value from Pardu's testing

--- IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ IntoTheDeep/Onyx2024/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -11,9 +11,9 @@ public final class Constants {
         public static final int viperBottomPosition=0;
 
         public static final int viperTopPosition=-3900;
-        public static final double bucketFlatPosition=0.85;
-        public static final double bucketDumpPosition=1;
-        public static final double bucketLoadPosition=0.6;
+        public static final double bucketFlatPosition=0.3;
+        public static final double bucketDumpPosition=0.5;
+        public static final double bucketLoadPosition=0.1;
         public static final double intakeMoveSpeed=0.7;
         public static final double wristMoveUpSpeed=0.5;
         public static final double wristMoveDownSpeed=-0.5;
